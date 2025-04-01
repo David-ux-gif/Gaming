ae508e111bb7eb94d51237148a60e28b238f4f55
David-ux-gif <invictus4044@gmail.com>
2025-04-01 19:00:00 +0545

    initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 function App() {
-  
-
   return (
-    <>
-      <p className='text-3xl font-bold underline '>Hello</p>
-    </>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
